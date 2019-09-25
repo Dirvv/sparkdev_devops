4227afd144a089291388b36aab11a925510dc00c
Nathercia Goncalves <ngonc003@fiu.edu>
2019-09-24 21:17:59 -0400

    Nat's pic

--- devops_app/src/App.js
+++ devops_app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NatherciaG from './NatherciaG';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <NatherciaG/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
