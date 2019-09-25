ee9c9840066144562d7d7d2e08151a048fffe3a8
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2019-09-24 21:12:42 -0400

    updated own fucntion with image

--- devops_app/src/App.js
+++ devops_app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JuanS from './JuanC';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <JuanS />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
