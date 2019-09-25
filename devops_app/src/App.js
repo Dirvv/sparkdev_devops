225d4abec6f2288ab5ab4858a74278f5f131b6c2
Andre Marin <andmar204@gmail.com>
2019-09-24 21:19:30 -0400

    Created AndreM component (used -a for this commit)

--- devops_app/src/App.js
+++ devops_app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AndreM from './AndreM';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+		<AndreM/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
