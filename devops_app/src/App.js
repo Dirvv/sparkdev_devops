7f1d7101a79d839e520e7655e1f0a916c37b24ce
mauricio s <msant323@fiu.edu>
2019-09-24 21:27:30 -0400

    cleanup merge junk

--- devops_app/src/App.js
+++ devops_app/src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
+
 import MauricioS from './MauricioS';
-=======
 import DavidA from './DavidA';
->>>>>>> a3b3367feccc76b1a04ad8a71cb160afeffca34f
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
         <MauricioS />
-=======
         <DavidA />
->>>>>>> a3b3367feccc76b1a04ad8a71cb160afeffca34f
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
