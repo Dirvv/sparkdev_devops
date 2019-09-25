c8f574beea7f3cf81b16ed5d7a845fe01262115d
mastercode35 <williamelias3@hotmail.com>
2019-09-24 21:16:46 -0400

    Adding new components for William

---
+++ devops_app/src/William.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function William() {
+  return (
+    <div className="William">
+      <p>William!!!</p>
+      <p>cereal when No milk</p>
+      <img src="https://i.kym-cdn.com/entries/icons/mobile/000/029/887/syoss89fhhf21.jpg" width="800" height="450" title="When Milk" alt="Milk"></img>
+      <p>CEREAL WHEN HAVES MILK</p>
+    </div>
+  );
+}
+
+export default William;
