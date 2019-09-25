4f718cb0772d76a6384f9ec9d378e019e358b966
adrian <cr7adrian@hotmail.com>
2019-09-24 21:13:37 -0400

    add unit test to adrian component

--- devops_app/src/AdrianS.test.js
+++ devops_app/src/AdrianS.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AdrianS from './AdrianS'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('render Adrian component without crashing', () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<AdrianS />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+
+
