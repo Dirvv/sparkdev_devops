b6ff31d82c95f02c89d0081c463d9204cff8875a
mastercode35 <williamelias3@hotmail.com>
2019-09-24 21:54:21 -0400

    Added Head

--- devops_app/src/William.js
+++ devops_app/src/William.js
@@ -7,6 +7,8 @@ function William() {
       <p>cereal when No milk</p>
       <img src="https://i.kym-cdn.com/entries/icons/mobile/000/029/887/syoss89fhhf21.jpg" width="800" height="450" title="When Milk" alt="Milk"></img>
       <p>CEREAL WHEN HAVES MILK</p>
+
+      <img src="https://media1.tenor.com/images/8d3e07f89eaab495081ca0a55e593a45/tenor.gif?itemid=10285425" title="So no head?" alt="Head"></img>
     </div>
   );
 }
