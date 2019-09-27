aa128df07517a939bb7f47be8c796d52c0653c5d
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2019-09-26 20:18:45 -0400

    updated my app fucntion

--- devops_app/src/JuanC.js
+++ devops_app/src/JuanC.js
@@ -4,7 +4,6 @@ function JuanC() {
   return (
     <div className="JuanC">
       <img height="600px" width="500px" src="https://k50.kn3.net/taringa/E/C/B/B/6/4/adriano034/2C4.jpg" alt="lol"></img>
-      <img  height="500px" width="500px" src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/21617619_274295403064371_9072578500703458324_n.jpg?_nc_cat=102&_nc_oc=AQlzDFP_C9m3_c1_MEoU9b_DWCL-x2Vqysa05S6xmZByQkFGn5uFDQV7rlA9egi_DAc&_nc_ht=scontent-mia3-2.xx&oh=7572261fbbae9a8e7aa4fbb53c1806a9&oe=5E3424EB" alt="lol"></img>
       <p>JuanC (:</p>
     </div>
   );
