4e5981ce367c6c7e67a3eb2d4df0ee28b7c5c9b5
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2019-09-26 21:22:56 -0400

    huh

--- devops_app/src/AdrianS.js
+++ devops_app/src/AdrianS.js
@@ -4,7 +4,7 @@ function AdrianS() {
   return (
     <div className="App">
       <p>Adrian S!!!</p>
-      <img src="https://www.bing.com/th?id=OIP.mE6kQg2C80tBBP6Ez_ULQQHaEx&pid=Api&rs=1"></img>
+      <img src="https://www.bing.com/th?id=OIP.mE6kQg2C80tBBP6Ez_ULQQHaEx&pid=Api&rs=1" alt="lol"></img>
     </div>
   );
 }
