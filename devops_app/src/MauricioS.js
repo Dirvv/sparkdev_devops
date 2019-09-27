7dd35ae1f8f4bc9c2df56072f8592aba7dda5f6d
David Acosta <dacos062@fiu.edu>
2019-09-26 20:56:08 -0400

    edit Mauricio.js image height

--- devops_app/src/MauricioS.js
+++ devops_app/src/MauricioS.js
@@ -4,7 +4,7 @@ function MauricioS() {
   return (
     <div className="MauricioS">
       <p>MauricioS!!!</p>
-      <img height="100" 
+      <img height="200" 
         src='https://screenrant.com/wp-content/uploads/2017/07/Rick-and-Morty.jpg' alt = 'Rick'/>
     </div>
   );
