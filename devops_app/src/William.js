4796977395cc8e015f939cbb5d3ad85b8c4cc3f0
mastercode35 <williamelias3@hotmail.com>
2019-09-26 20:57:55 -0400

    Modified some text on my app.

--- devops_app/src/William.js
+++ devops_app/src/William.js
@@ -3,7 +3,7 @@ import React from 'react';
 function William() {
   return (
     <div className="William">
-      <p>William!!!</p>
+      <p>This is William's chunck of the application!</p>
       <p>cereal when No milk</p>
       <img src="https://i.kym-cdn.com/entries/icons/mobile/000/029/887/syoss89fhhf21.jpg" width="800" height="450" title="When Milk" alt="Milk"></img>
       <p>CEREAL WHEN HAVES MILK</p>
